1f2be4b131977e66c6ee0fd3c8a378ef6a2c1339
David Bernheisel <david@bernheisel.com>
2018-03-26 08:44:39 -0400

    Tests for Assignment

---
+++ test/exgradebook/curriculum/assignment_test.exs
@@ -0,0 +1,79 @@
+defmodule Exgradebook.Curriculum.AssignmentTest do
+  use Exgradebook.DataCase, async: true
+  alias Exgradebook.Curriculum
+  alias Exgradebook.Curriculum.Assignment
+
+  describe "changeset" do
+    test "requires fields" do
+      params = params_for(
+        :assignment,
+        name: nil,
+        value: nil,
+        course: nil
+      )
+
+      changeset = Assignment.changeset(%Assignment{}, params)
+
+      assert changeset.errors[:name]
+      assert changeset.errors[:value]
+      assert changeset.errors[:course_id]
+    end
+  end
+
+  describe "list_assignments" do
+    test "returns all assignments" do
+      assignments = insert_pair(:assignment)
+
+      [fetched_assignment_one, fetched_assignment_two] = Curriculum.list_assignments()
+
+      assert fetched_assignment_one.id in map_ids(assignments)
+      assert fetched_assignment_two.id in map_ids(assignments)
+    end
+  end
+
+  describe "list_assignments_for_course" do
+    test "returns all assignments for course" do
+      course = insert(:course)
+      assignments = insert_pair(:assignment, course: course)
+      _other_assignment = insert(:assignment)
+
+      [fetched_assignment_one, fetched_assignment_two] = Curriculum.list_assignments_for_course(course.id)
+
+      assert fetched_assignment_one.id in map_ids(assignments)
+      assert fetched_assignment_two.id in map_ids(assignments)
+    end
+
+    test "returns all assignments scoped to staff for course" do
+      other_user = insert(:teacher)
+      user = insert(:teacher)
+      course = insert(:course, teacher: other_user)
+      _assignments = insert_pair(:assignment, course: course)
+      _other_assignment = insert(:assignment)
+
+      assert [] = Curriculum.list_assignments_for_course(course.id, user: user)
+    end
+
+    test "returns all assignments scoped to student for course" do
+      student = insert(:student)
+      course = insert(:course)
+      _other_course = insert(:course)
+      _assignments = insert_pair(:assignment, course: course)
+      insert(:enrollment, student: student, course: course)
+      _other_assignment = insert(:assignment)
+
+      assert [] = Curriculum.list_assignments_for_course(course.id, user: student)
+    end
+  end
+
+  describe "points possible for course" do
+    test "returns the sum of the assignments for the course" do
+      course = insert(:course)
+      _assignments = insert_pair(:assignment, course: course, value: 41)
+      _other_assignment = insert(:assignment, value: 30)
+
+      points_possible = Curriculum.points_possible_for_course(course.id)
+
+      assert points_possible == 82
+    end
+  end
+end
